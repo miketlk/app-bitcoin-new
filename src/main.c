5edcd0a72936a7fdf45032ce908618329cfb5a4f
Mike Tolkachev <mstolkachev@gmail.com>
2025-04-14 17:54:19 -0300

    Change testnet and regtest asset names to 'tl-BTC'

--- src/main.c
+++ src/main.c
@@ -205,8 +205,8 @@ void coin_main() {
 
     _Static_assert(sizeof(cx_sha256_t) <= 108, "cx_sha256_t too large");
     _Static_assert(sizeof(policy_map_key_info_t) <= 156, "policy_map_key_info_t too large");
-    // we assume in display.c that the ticker size is at most 5 characters (+ null)
-    _Static_assert(sizeof(COIN_COINID_SHORT) <= 6, "COIN_COINID_SHORT too large");
+    // Ensure that the coin name is no longer than supported asset ticker length.
+    _Static_assert(sizeof(COIN_COINID_SHORT) <= (MAX_ASSET_TICKER_LENGTH + 1), "COIN_COINID_SHORT too large");
 
 #if defined(HAVE_PRINT_STACK_POINTER) && defined(HAVE_BOLOS_APP_STACK_CANARY)
     PRINTF("STACK CANARY ADDRESS: %08x\n", &app_stack_canary);
